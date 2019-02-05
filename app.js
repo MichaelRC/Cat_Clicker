686e331eb2ba1cec5998c63ef541aea6b318f7ff
Michael R Campitiello <michaelrcampi@gmail.com>
2019-02-05 13:56:03 -0800

    Got JS one picture click working

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-let clicks = 0;
-const img = document.getElementsByClassName('img');
+/*Gobal Variables*/
+
+// Take image and store in elem
+var elem = document.getElementById('img');
+// Take clicker HTML ID and store it in ClickCount
+var clickCount = document.getElementById('num_clicks'); 
+// Variable to store number of clicks.
+var clicks = 0;
+
+
+/*Click Iteration Function*/
+elem.addEventListener('click', function () {
+    //Every Click add 1 (clicks = clicks + 1)
+    clicks += 1;
+    // Apply number of clicks to clickCount ID so number 
+    // changeson screen
+    clickCount.innerHTML = clicks;
+}, false);
 
